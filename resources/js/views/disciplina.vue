b065ded1577e4faa2d77eab99d182541b7995866
JocelynePT <56885388+JocelynePT@users.noreply.github.com>
2020-05-22 17:48:29 +0100

    Template atualizado

--- resources/js/views/disciplina.vue
+++ resources/js/views/disciplina.vue
@@ -1,6 +1,18 @@
 <template>
-   <div>
-       <h1>Nome da disciplina</h1>
+    <div>
+      <div class="main-panel">
+          <div class="content-wrapper">
+            <div>
+                <h1>Nome da disciplina</h1>
+            </div>
+            <div class="section">
+                <h3>Informações gerais</h3>
+                <p>Texto da descrição da disciplina</p>
+            </div>
+            <div class="section">
+                <h2>Últimas atualizações</h2> 
+            </div>    
+        </div> 
     </div>
 </template>
 
