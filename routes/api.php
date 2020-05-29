688f2ce4b57c9c02caedd9a464aa85d850c1645f
Bruno Ribeiro <contact@nullscope.com>
2020-05-29 01:53:11 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::apiResource('alunos', 'API\AlunoController');
 Route::resource('alunos.disciplinas', 'API\AlunoDisciplinaController')->only([
