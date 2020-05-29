872534b2035641b8cdd005841622bcdf9290fb5b
Bruno Ribeiro <contact@nullscope.com>
2020-05-29 01:57:52 +0100

    Removed closure route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function () {
-    return view('layouts/app');
-})->where('any', '.*');
+Route::get('/{any}', 'AppController@index')->where('any', '.*');
 
 Auth::routes();
